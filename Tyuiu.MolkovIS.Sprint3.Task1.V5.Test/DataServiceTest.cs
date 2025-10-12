b9ae58a84b8cf8a36895313af17ba84d1148a57d
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-10-12 14:59:22 +0500

    Task1

---
+++ Tyuiu.MolkovIS.Sprint3.Task1.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MolkovIS.Sprint3.Task1.V5.Lib;
+
+namespace Tyuiu.MolkovIS.Sprint3.Task1.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double wait = 3.195;
+            double real = ds.GetMultiplySeries(1, 10);
+            Assert.AreEqual(wait, real);
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint3.Task1.V5/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MolkovIS.Sprint3.Task1.V5.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        DataService dataService = new DataService();
+        Console.Write("Введите начало: "); int start = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите конец: "); int end = Convert.ToInt32(Console.ReadLine());
+        double res = dataService.GetMultiplySeries(start, end);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
