0f06fc7ef3d3e55e0799b33a46b19907229619b9
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-10-14 13:42:03 +0500

    Task4

---
+++ Tyuiu.MolkovIS.Sprint3.Task4.V12.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+
+namespace Tyuiu.MolkovIS.Sprint3.Task4.V12.Lib
+{
+    public class DataService : ISprint3Task4V12
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 1;
+            for (int i = startValue; i < stopValue; i++)
+            {
+                if (i == 0) break;
+                res *= i / (i + Math.Sin(i)) + 2.5;
+            }
+            return Math.Round(res,3);
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint3.Task4.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MolkovIS.Sprint3.Task4.V12.Lib;
+
+namespace Tyuiu.MolkovIS.Sprint3.Task4.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            
+            double wait = 467.590;
+            double real = dataService.Calculate(-5, 5);
+            Assert.AreEqual(wait, real);
+
+        }
+    }
+}
