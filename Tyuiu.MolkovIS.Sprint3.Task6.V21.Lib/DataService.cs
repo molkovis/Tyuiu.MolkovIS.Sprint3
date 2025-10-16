bbb7a89c51a7317fd3801704a0f47b5d82071f0a
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-10-16 20:24:28 +0500

    Task6

---
+++ Tyuiu.MolkovIS.Sprint3.Task6.V21.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MolkovIS.Sprint3.Task6.V21.Lib
+{
+    public class DataService : ISprint3Task6V21
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int sum = 0;
+            for (int start = startValue; start <= stopValue; start++)
+            {
+                for (int del = 1; del <= start; del++)
+                {
+                    if (start % del == 0) sum+= del;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint3.Task6.V21.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MolkovIS.Sprint3.Task6.V21.Lib;
+
+namespace Tyuiu.MolkovIS.Sprint3.Task6.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            int start = 4;
+            int stop = 14;
+            int wait = 157;
+            int real = dataService.GetSumTheDivisors(start, stop);
+            Assert.AreEqual(wait, real);
+
+        }
+    }
+}
